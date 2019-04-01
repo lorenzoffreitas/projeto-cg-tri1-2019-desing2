338fe3461ab90834c3b4ea4d23f6ae16a132766a
lorenzoffreitas <lorenzo_freitas@hotmail.com>
2019-04-01 09:26:02 -0300

    Tocando o som da moeda com o AudioManager e AudioSFX

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.CompareTag("Coin")){
             SFXManager.instance.ShowCoinParticles(other.gameObject);
+            AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
             Destroy(other.gameObject);
         }
     }
